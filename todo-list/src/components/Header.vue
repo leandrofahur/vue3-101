f86f6b3ca52b63dd2f5ea0a2246a65eeadbc9e97
Leandro Machado <fahurleandro@gmail.com>
2022-04-11 17:47:44 -0700

    tasks

--- todo-list/src/components/Header.vue
+++ todo-list/src/components/Header.vue
@@ -1,16 +1,26 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green" @click="onClick()" />
   </header>
 </template>
 
 <script>
+import Button from "./Button";
 export default {
   name: "Header",
   props: {
     title: {
       type: String,
       default: "Task Tracker",
+    },
+  },
+  components: {
+    Button,
+  },
+  methods: {
+    onClick() {
+      console.log("Clicked!!!");
     },
   },
 };
